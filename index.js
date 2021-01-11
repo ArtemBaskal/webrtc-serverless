7e647e002f3bf9f1e56a9ca4c9b77a54eebd8643
Artem Baskal <a.baskal@adguard.com>
2021-01-11 22:56:13 +0300

    Add data channels for every side

--- index.js
+++ index.js
@@ -15,6 +15,9 @@ class SignalingChannel {
 }
 
 const QUERY_PARAM_ROOM_NAME = 'room';
+const FILE_DATA_CHANNEL_BINARY_TYPE = 'arraybuffer';
+const MESSAGES_CHANNEL_NAME = 'sendDataChannel';
+const END_OF_FILE_MESSAGE = 'EOF';
 const generateQueryParam = (queryParamKey, queryParamValue) => new URLSearchParams({[queryParamKey]: queryParamValue}).toString();
 
 const main = (room) => {
@@ -40,6 +43,131 @@ const main = (room) => {
     };
 
     const pc = new RTCPeerConnection(configuration);
+
+    const onSendChannelStateChange = (e) => {
+        const readyState = e.target.readyState;
+        console.log('Send channel state is: %s', readyState);
+    };
+
+    let chatChannel;
+    const createChatChannel = () => {
+        if (chatChannel) {
+            return;
+        }
+
+        const dataConstraint = null;
+        chatChannel = pc.createDataChannel(MESSAGES_CHANNEL_NAME, dataConstraint);
+        chatChannel.addEventListener('open', onSendChannelStateChange);
+        chatChannel.addEventListener('close', onSendChannelStateChange);
+        dataChannelSenderBtn.addEventListener('click', () => {
+            chatChannel.send(dataChannelSender.value);
+        });
+    };
+
+    dataChannelSender.addEventListener('focus', createChatChannel);
+
+    dataChannelFileSenderBtn.addEventListener('click', () => {
+        dataChannelFileSender.click();
+    });
+
+    dataChannelFileSender.addEventListener('change', (e) => {
+        const [file] = e.target.files;
+        const {name, size} = file;
+        sendProgress.max = size;
+
+        /*
+        Label may not be longer than 65,535 bytes.
+        https://developer.mozilla.org/en-US/docs/Web/API/RTCPeerConnection/createDataChannel#parameters:~:text=channel.%20This%20string-,may%20not%20be%20longer%20than%2065%2C535%20bytes.
+        Filename sizes may not exceed 255 bytes.
+        https://www.ibm.com/support/knowledgecenter/SSEQVQ_8.1.10/client/c_cmd_filespecsyntax.html
+        */
+        const dataConstraint = null;
+        const sendFileChannel = pc.createDataChannel(name, dataConstraint);
+        sendFileChannel.binaryType = FILE_DATA_CHANNEL_BINARY_TYPE;
+        /*
+         Firefox cannot send a message larger than 16 Kbytes to Chrome
+         https://viblast.com/blog/2015/2/5/webrtc-data-channel-message-size/#blog-body:~:text=Firefox%20cannot%20send%20a%20message%20larger%20than%2016%20Kbytes%20to%20Chrome
+         Messages smaller than 16kiB can be sent without concern, as all major user agents handle them the same way. Beyond that, things get more complicated.
+         https://developer.mozilla.org/en-US/docs/Web/API/WebRTC_API/Using_data_channels#understanding_message_size_limits:~:text=Messages%20smaller%20than%2016kiB%20can%20be,Beyond%20that%2C%20things%20get%20more%20complicated.
+        */
+        const CHUNK_SIZE = 2 ** 14;
+        const FIRST_SLICE_BYTE = 0;
+
+        const fileReader = new FileReader();
+        let offset = 0;
+
+        fileReader.addEventListener('error', (error) => {
+            console.error('Error reading file: ', error)
+        });
+
+        fileReader.addEventListener('abort', (event) => {
+            console.log('File reading aborted: ', event);
+        });
+
+        const readSlice = (byteOffset) => {
+            console.log('readSlice', byteOffset);
+            const slice = file.slice(offset, byteOffset + CHUNK_SIZE);
+            fileReader.readAsArrayBuffer(slice);
+        };
+
+        fileReader.addEventListener('load', (e) => {
+            console.log('FileReader.onload', e);
+            sendFileChannel.send(e.target.result);
+
+            offset += e.target.result.byteLength;
+            sendProgress.value = offset;
+
+            if (offset < file.size) {
+                readSlice(offset);
+            } else {
+                sendFileChannel.send(END_OF_FILE_MESSAGE);
+            }
+        });
+
+        readSlice(FIRST_SLICE_BYTE);
+    });
+
+    pc.addEventListener('datachannel', ({channel}) => {
+            console.log('Receive Channel Callback');
+
+            if (channel.label === MESSAGES_CHANNEL_NAME) {
+                channel.addEventListener('message', (event) => {
+                    console.log('Received Message: %s', event.data);
+                    sendDataChannelReceiver.value = event.data;
+                });
+            } else {
+                const fileName = channel.label;
+                let receivedFileBuffer = [];
+                let receivedFileSize = 0;
+
+                channel.binaryType = FILE_DATA_CHANNEL_BINARY_TYPE;
+
+                channel.addEventListener('message', (event) => {
+                    if (event.data === END_OF_FILE_MESSAGE) {
+                        /* FIXME does this understand type application/sdp? */
+                        const receivedFile = new Blob(receivedFileBuffer,/* {type: 'application/sdp'}*/);
+                        receivedFileBuffer = [];
+                        receivedFileSize = 0;
+
+                        const a = dataChannelFileDownloadLink;
+                        const url = URL.createObjectURL(receivedFile);
+                        a.href = url;
+                        a.download = fileName;
+                        a.textContent = `Click to download ${fileName} (${receivedFile.size} bytes)`;
+                        // a.click();
+                        // URL.revokeObjectURL(url);
+                        // a.remove()
+                        channel.close();
+                    } else {
+                        receivedFileBuffer.push(event.data);
+                        receivedFileSize += event.data.byteLength;
+                    }
+                });
+            }
+            channel.addEventListener('open', onSendChannelStateChange);
+            channel.addEventListener('close', onSendChannelStateChange);
+        }
+    );
 
 // call start() anytime on either end to add camera and microphone to connection
     const start = async () => {
@@ -93,6 +221,12 @@ const main = (room) => {
             console.error(err);
         } finally {
             makingOffer = false;
+        }
+    };
+
+    pc.oniceconnectionstatechange = () => {
+        if (pc.iceConnectionState === "failed") {
+            pc.restartIce();
         }
     };
 
